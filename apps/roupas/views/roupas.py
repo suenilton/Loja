c2cd3a13a552696f762519bdd5f4301bc7279bec
suenilton <sueniltonjunior@gmail.com>
2022-06-05 10:24:34 -0300

    paginação

--- apps/roupas/views/roupas.py
+++ apps/roupas/views/roupas.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
 from roupas.models import *
 
 def home(request):
@@ -14,15 +15,22 @@ def home(request):
 
 def produtos(request, **kwargs):
     categoria = None
+
     produtos = Produto.objects.order_by('-data_produto').filter(status_produto=True)
     categorias = Categoria.objects.all()
+
     categoria_slug = kwargs.get('slug')
     if categoria_slug:
         categoria = get_object_or_404(Categoria, slug=categoria_slug)
         produtos = produtos.filter(categoria_produto=categoria)
+
+    paginator = Paginator(produtos, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'produtos': produtos,
-        'categorias': categorias
+        'produtos': page_obj,
+        'categorias': categorias,
     }
     
     return render(request, 'loja\produtos.html', context)
