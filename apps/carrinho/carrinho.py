6a0e6ebbc768969b57f7f6e8e452f14a1cec38a9
suenilton <sueniltonjunior@gmail.com>
2022-06-21 18:04:55 -0300

    continuando os testes

--- apps/carrinho/carrinho.py
+++ apps/carrinho/carrinho.py
@@ -1,3 +1,15 @@
+from statistics import quantiles
+
+
 class Carrinho():
     def __init__(self, request):
-        request.session['carrinho'] = {}
+        if request.session.get('carrinho') is None:
+            request.session['carrinho'] = {}
+        
+        self.carrinho = request.session['carrinho']
+    
+    def add(self, product):
+        self.carrinho[str(product.id)] = {
+            'quantity' : 1,
+            'price' : str(product.price),
+        }
